97496039e2de780146b7af200945d0e7949304a5
unknown <matt.mcquain@gmail.com>
2021-10-26 20:16:55 -0700

    navbar hamburger menu changes to X icon on click

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,11 @@
-console.log('yo');
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function () {
+  console.log('click hamburger');
+
+  if (btnHamburger.classList.contains('open')) {
+    btnHamburger.classList.remove('open');
+  } else {
+    btnHamburger.classList.add('open');
+  }
+});
