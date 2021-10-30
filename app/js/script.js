35af6223a56b1623718de2a780eea9826da7ba3b
unknown <matt.mcquain@gmail.com>
2021-10-29 20:10:16 -0700

    nav overylay animates correctly. Starting on the actual layout next

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,8 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 
 btnHamburger.addEventListener('click', function () {
@@ -8,12 +10,19 @@ btnHamburger.addEventListener('click', function () {
 
   // when you click the button, we check if the header has the open class. If it doesnt have it, add it. If it does have it, remove it. 
   if (header.classList.contains('open')) { // Close hamburger menu
+    body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
-  } else { // Open hamburger menu
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+  } 
+  else { // Open hamburger menu
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.add('fade-in');
-    overlay.classList.remove('fade-out');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
   }
 });
